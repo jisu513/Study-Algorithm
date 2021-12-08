69de1ce8ed6ee34e75cc061c7fd44e4fb5a9159f
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2021-12-08 18:59:45 +0900

    Section1 2번 문제

---
+++ Algo/src/Inflearn/Section1/S02.java
@@ -0,0 +1,29 @@
+package Inflearn.Section1;
+
+import java.io.*;
+
+public class S02 { //대소문자 변환
+	public String solution(String str) {
+		String answer = "";
+
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isLowerCase(str.charAt(i))) { 
+				answer += Character.toUpperCase(str.charAt(i));
+			} else {
+				answer += Character.toLowerCase(str.charAt(i));
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) throws IOException {
+		S02 M = new S02();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		System.out.print(M.solution(str));
+
+	}
+
+}
