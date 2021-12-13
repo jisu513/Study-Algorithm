8b5ace7c3a8c5206eb99a508707ef7d482088e04
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2021-12-13 21:24:11 +0900

    9번 문제 풀이

---
+++ Algo/src/Inflearn/Section1/S09.java
@@ -0,0 +1,25 @@
+package Inflearn.Section1;
+
+import java.io.*;
+
+public class S09 {
+	public int solution(String str) {
+		int answer = 0;
+
+		str = str.replaceAll("[^0-9]", "");
+		answer = Integer.parseInt(str);
+
+		return answer;
+	}
+
+	public static void main(String[] args) throws IOException {
+		S09 S = new S09();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+
+		System.out.println(S.solution(str));
+
+	}
+
+}
