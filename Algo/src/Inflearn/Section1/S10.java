9afb896bf44aa0c9f8afee7c2ae0208d94d319a3
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2021-12-17 21:41:14 +0900

    설명 추가

--- Algo/src/Inflearn/Section1/S10.java
+++ Algo/src/Inflearn/Section1/S10.java
@@ -11,7 +11,7 @@ public class S10 {// 가장 짧은 문자거리(문자열의 최대 길이는 100)
 		int cnt = 1000; 
 		// 처음 cnt를 큰 숫자로 잡아 놓은 것이 중요! -> cnt=0 일 경우 첫번째 문자열은 항상 최소값이므로 오답!
 
-		for (int i = 0; i < s.length(); i++) {
+		for (int i = 0; i < s.length(); i++) { // 왼쪽 -> 오른쪽 최소거리 비교
 			cnt++;
 			if (s.charAt(i) == t) {
 				cnt = 0;
@@ -21,7 +21,7 @@ public class S10 {// 가장 짧은 문자거리(문자열의 최대 길이는 100)
 
 		cnt = 1000;
 	    
-		for (int i = s.length() - 1; i >= 0; i--) {
+		for (int i = s.length() - 1; i >= 0; i--) { // 오른쪽 -> 왼쪽 최소거리 비교
 			cnt++;
 			if (s.charAt(i) == t) {
 				cnt = 0;
