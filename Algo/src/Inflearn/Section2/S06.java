1883a117d64dca066a2c1f7e5b31707558f4d356
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2021-12-18 22:05:59 +0900

    6번 문제 해결

--- Algo/src/Inflearn/Section2/S06.java
+++ Algo/src/Inflearn/Section2/S06.java
@@ -3,10 +3,37 @@ package Inflearn.Section2;
 import java.io.*;
 import java.util.*;
 
-public class S06 {
+public class S06 {// 뒤집은 소수
+	public boolean isPrime(int n) {//소수인지 판별!
+		if (n == 1) {
+			return false;
+		}
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	public ArrayList<Integer> solution(int n, int[] arr) {
 		ArrayList<Integer> answer = new ArrayList<>();
-		
+
+		for (int i = 0; i < n; i++) {
+			int tmp = arr[i];
+			int rev = 0;
+
+			while (tmp > 0) { // ***숫자 뒤집기***
+				int t = tmp % 10; // tmp의 나머지(마지막 자리 숫자)
+				rev = rev * 10 + t;
+				tmp = tmp / 10; // tmp의 몫
+			}
+			if (isPrime(rev)) {
+				answer.add(rev);
+			}
+
+		}
+
 		return answer;
 	}
 
@@ -14,16 +41,17 @@ public class S06 {
 		S06 S = new S06();
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-
+		
 		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = null;
 		st = new StringTokenizer(br.readLine());
 		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		for(int x : S.solution(n, arr)) {
+
+		for (int x : S.solution(n, arr)) {
 			System.out.print(x + " ");
 		}
 
