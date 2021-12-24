ad26c2f81217d224645cda20cb6467043b1026d1
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2021-12-24 23:50:39 +0900

    Section3 1번 문제 해결

---
+++ Algo/src/Inflearn/Section3/S01.java
@@ -0,0 +1,63 @@
+package Inflearn.Section3;
+
+import java.io.*;
+import java.util.*;
+
+public class S01 {//두 배열 합치기(two pointers algorithm)
+	public ArrayList<Integer> solution(int n, int m, int[] narr, int[] marr){
+		ArrayList<Integer> answer = new ArrayList<>();
+		
+		int p1=0, p2=0;
+		
+		//narr과 marr 크기 비교후 작은 것 먼저 add
+		while(p1 < n && p2 < m) {
+			if(narr[p1] < marr[p2]) {
+				answer.add(narr[p1]);
+				p1++;
+			}else {
+				answer.add(marr[p2]);
+				p2++;
+			}
+		}
+		
+		//비교 후 남은 배열 add
+		while(p1 < n) {
+			answer.add(narr[p1]);
+			p1++;
+		}
+		
+		while(p2 < m) {
+			answer.add(marr[p2]);
+			p2++;
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args) throws IOException {
+		S01 S = new S01();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		int n = Integer.parseInt(br.readLine());
+		int[] narr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			narr[i] = Integer.parseInt(st.nextToken()); 
+		}
+		
+		int m = Integer.parseInt(br.readLine());
+		int[] marr = new int[m];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<m; i++) {
+			marr[i] = Integer.parseInt(st.nextToken()); 
+		}
+		
+		for(int x : S.solution(n, m, narr, marr)) {
+			System.out.print(x + " ");
+		}
+
+	}
+
+}
