fb3e71781a8215cb5512f5deb4d6337cf20ad451
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2021-12-31 00:03:19 +0900

    다시 풀어봤지만.. 한 번 더 다시 풀기

--- Algo/src/Inflearn/Section3/S36.java
+++ Algo/src/Inflearn/Section3/S36.java
@@ -7,13 +7,13 @@ public class S36 {// 최대길이 연속부분수열
 	public int solution(int n, int k, int[] arr) {
 		int answer = 0;
 
-		int cnt = 0, lt = 0, len = 0;
+		int cnt = 0, len = 0, lt = 0;
 		for (int rt = 0; rt < n; rt++) {
 			if (arr[rt] == 0) {
 				cnt++;
 			}
 			while (cnt > k) {
-				if (arr[lt] == 0) {
+				if (arr[lt] == 0) { // arr[lt]의 값이 0이면 cnt값 하나 빼줌!(cnt > k 라고 무조건 하나 빼는거 아님!)
 					cnt--;
 				}
 				lt++;
