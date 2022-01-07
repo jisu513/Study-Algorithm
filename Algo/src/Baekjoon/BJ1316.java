e01420bea8d87e7ba636476e7945e9c2c79c4a18
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-01-07 23:23:38 +0900

    실패-다시풀기

---
+++ Algo/src/Baekjoon/BJ1316.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ1316 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int num = Integer.parseInt(br.readLine());
+		String s;
+		for (int i = 0; i < num; i++) {
+			s = br.readLine();
+			System.out.println(s);
+		}
+		
+		
+		
+
+	}
+
+}
