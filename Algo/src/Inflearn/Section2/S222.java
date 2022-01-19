7ff7ad0f66b5d7cbac9f62530738024fd7171ecb
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-01-19 22:37:58 +0900

    문제 해결(다시 풀어봄)

--- Algo/src/Inflearn/Section2/S222.java
+++ Algo/src/Inflearn/Section2/S222.java
@@ -12,11 +12,11 @@ public class S222 {// 멘토링
 
 		// 4중 for문 사용
 		for (int i = 1; i <= n; i++) {// n:학생 수 i=멘토
-			for (int j = 1; j <= n; j++) {// n:학생 수 j=멘티 --> (i, j) = (멘토, 멘티) 경우의 수
+			for (int j = 1; j <= n; j++) {//     j=멘티 --> (i, j) = (멘토, 멘티) 경우의 수
 				int cnt = 0;
 				for (int k = 0; k < m; k++) {// m:test 수
 					int pi = 0, pj = 0;
-					for (int s = 0; s < n; s++) {// n:등 수 --> 학생 수 = 등수(당연한 소리지만.. 헷갈림)
+					for (int s = 0; s < n; s++) {// n:등 수 --> 학생 수 = 등수(당연한 소리지만.. 헷갈렸음)
 						if (arr[k][s] == i) {// arr[k][s]의 값은 멘토/멘티 학생번호!
 							pi = s; // i는 s등
 							// -->arr[k][s]의 값이 i의 값과 동일 할 때의 s값이 i의 등수!
@@ -26,11 +26,11 @@ public class S222 {// 멘토링
 						}
 
 					}
-					if (pi < pj) {// 등수는 값이 높은 등수!(헷갈림 주의)
+					if (pi < pj) {// 등수는 값이 낮은게 높은 등수!(헷갈림..)
 						cnt++;
 					}
 				}
-				if (cnt == m) {// cnt의 값과 test수가 같아야 모든 test에서 i가 j보다 높은 등수 --> 멘토/멘티 가능!
+				if (cnt == m) {// cnt의 값과 test수가 같아야 모든 test에서 i가 j보다 높은 등수 --> 멘토링 가능!
 					answer++;
 				}
 
