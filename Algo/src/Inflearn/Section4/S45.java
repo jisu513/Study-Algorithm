095c75217af1634733d74b25e91af8efab0d4356
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-01-25 23:27:11 +0900

    TreeSet import 오류 - 미해결

--- Algo/src/Inflearn/Section4/S45.java
+++ Algo/src/Inflearn/Section4/S45.java
@@ -3,13 +3,20 @@ package Inflearn.Section4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class S45 {
+
+
+
+public class S45 {//k번째 큰 수
 	public int solution(int n, int k, int[] arr) {
 		int answer = -1;
 		
-		//TreeSet 사용!
+		//TreeSet 사용! (중복제거와 정렬)
+		/* TreeSet: Set 인터페이스를 구현한 클래스! 객체를 중복해서 저장 할 수 없고 저장 순서가 유지되지 않음 (Set의 성질)
+		         -> 이진 탐색 트리 구조로 이루어짐 (검색능력을 더 향상시킨 레드-블랙 트리)
+		  */
+		//Set<Integer> Tset = new TreeSet<>(Collections.reverseOrder()); //내림차순 정렬 (디폴트는 오름차순 정렬)
 		
 		return answer;
 	}
