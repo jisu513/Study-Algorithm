fa9674bc9efd71e76d7519ce128537dc4de81393
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-01-27 00:04:39 +0900

    다시 풀어보기

--- Algo/src/Inflearn/Section4/S45.java
+++ Algo/src/Inflearn/Section4/S45.java
@@ -12,13 +12,29 @@ public class S45 {//k번째 큰 수
 	public int solution(int n, int k, int[] arr) {
 		int answer = -1;
 		
-		//TreeSet 사용! (중복제거와 정렬)
+		//TreeSet 사용! (중복 제거와 정렬)
 		/* TreeSet: Set 인터페이스를 구현한 클래스! 객체를 중복해서 저장 할 수 없고 저장 순서가 유지되지 않음 (Set의 성질)
 		         -> 이진 탐색 트리 구조로 이루어짐 (검색능력을 더 향상시킨 레드-블랙 트리)
 		  */
 		TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder()); //내림차순 정렬 (디폴트는 오름차순 정렬)
 		
-		return answer;
+		// 문제에서 3개의 합을 구하라 했으므로 3중 for문 사용!
+		for(int i = 0; i<n; i++) {
+			for(int j = i+1; j<n; j++) {
+				for(int l = j+1; l<n; l++) {
+					Tset.add(arr[i]+arr[j]+arr[l]); 
+					//3장씩 뽑은 카트 합의 모든 경우의 수 Tset에 큰 수 부터 정렬되어 있음!
+				}
+			}
+		}
+		int cnt=0;
+		for(int x : Tset) {
+			cnt++;
+			if(cnt==k) {
+				return x; //k번째 값이 있으면 x return
+			}
+		}
+		return answer;//k번째 값이 없으면 1 return
 	}
 
 	public static void main(String[] args) throws IOException {
