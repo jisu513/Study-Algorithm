3630d1fc2f7c6711dff829ec2758c62d88a1fecc
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-01-30 23:26:27 +0900

    복습- 문제 해결

--- Algo/src/Inflearn/Section5/S53.java
+++ Algo/src/Inflearn/Section5/S53.java
@@ -12,24 +12,25 @@ public class S53 {// 인형 뽑기
 
 		Stack<Integer> sk = new Stack<>();
 
-		for (int pos : moves) {
-			for (int j = 1; j < board.length; j++) { // board의 행 길이! 열 길이는 board[0].length!
-				if (board[j][pos] != 0) {//1번
-					int doll = board[j][pos];
-					board[j][pos] = 0;
-					if (!sk.isEmpty() && doll == sk.peek()) {
-						//stack이 비어있지 않을 때 비교
+		for (int pos : moves) {// moves의 배열값에 해당하는 board의 index로 이동!
+			for (int i = 0; i < board.length; i++) {// board의 행 길이! 열 길이는 board[0].length!
+				if (board[i][pos - 1] != 0) {// board의 index는 0부터 시작하므로 -1 해줘야함!
+					int doll = board[i][pos - 1];
+					board[i][pos - 1] = 0;
+					if (!sk.isEmpty() && sk.peek() == doll) { //stack이 비어있지 않을 때 비교
 						//peek(): stack의 제일 상단에 있는 값만 확인!(pop()은 stack에서 꺼내서 리턴받음!)
 						answer += 2;
 						sk.pop();
 					} else {
 						sk.push(doll);
-					}
-					break; //**중요! 1번의 if문이 참이면 for문 한 번 수행 후 멈추기! 그 다음까지 넘어가면 안됨!
-				}
 
+					}
+					break; //if가 참일 때 하나만 수행! break 안해주면 for문 계속 돌게 되므로 오답!
+				}
 			}
+
 		}
+
 		return answer;
 	}
 
@@ -40,10 +41,10 @@ public class S53 {// 인형 뽑기
 		StringTokenizer st = null;
 
 		int n = Integer.parseInt(br.readLine());
-		int[][] board = new int[n+1][n+1];
-		for (int i = 1; i <= n; i++) {
+		int[][] board = new int[n + 1][n + 1];
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= n; j++) {
+			for (int j = 0; j < n; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
