7eb0f501abe1dffa14b59050a07e01fe5b940fe3
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-01-30 23:08:54 +0900

    해결 못함 - 다시 풀기

--- Algo/src/Baekjoon/BJ9012.java
+++ Algo/src/Baekjoon/BJ9012.java
@@ -4,18 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BJ9012 {
+public class BJ9012 {//°ýÈ£
+	public static String solution(String s) {
+		String answer="Yes";
+		return answer;
+	}
 
 	public static void main(String[] args) throws IOException {
+		BJ9012 B = new BJ9012();
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int num = Integer.parseInt(br.readLine());
-		String s;
-		for (int i = 0; i < num; i++) {
-			s = br.readLine();
-			System.out.println(s);
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0; i<n; i++) {
+			sb.append(solution(br.readLine())).append('\n');
 		}
+		
+		System.out.println(sb);
+
+		
 		
 		
 		
