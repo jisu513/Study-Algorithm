afff3fd98863f81cec0876a06bb585c995b9c052
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-02 22:30:48 +0900

    문제 해결!(다시풀기)

---
+++ Algo/src/Inflearn/Section5/S55.java
@@ -0,0 +1,39 @@
+package Inflearn.Section5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class S55 {
+	public int solution(String s) {
+		int answer = 0;
+		
+		Stack<Character> sk = new Stack<>();
+		for(int i = 0; i<s.length(); i++) {
+			if(s.charAt(i) == '(') {
+				sk.push(s.charAt(i));
+			}else {
+				sk.pop();
+				if(s.charAt(i-1) == '(') { //레이저 일 때 -->()모양일 때
+					answer += sk.size();
+				}else {//막대기의 끝일 때
+					answer++;
+				}
+			}
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args) throws IOException {
+		S55 S = new S55();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		System.out.println(S.solution(s));
+
+	}
+
+}
