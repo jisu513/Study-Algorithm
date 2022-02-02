ac9f30a9b7faf145a947c8e234a0b774dfd21df8
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-02 21:57:37 +0900

    복습- 문제 해결

--- Algo/src/Inflearn/Section5/S54.java
+++ Algo/src/Inflearn/Section5/S54.java
@@ -10,13 +10,15 @@ public class S54 {// 후위식 연산
 		int answer = 0;
 
 		Stack<Integer> sk = new Stack<>();
-		for (int x : s.toCharArray()) {
-			if (Character.isDigit(x)) {
+		
+		for(char x : s.toCharArray()) {
+			if(Character.isDigit(x)) {
 				sk.push(x - 48);
-				//문자를 숫자로 변환하려면 48을 빼주면 됨!(48은 '0'(문자 0)의 아스키코드))
+				//문자 타입의 숫자(char)를 숫자 타입(int)으로 바꾸려면 48을 빼면됨! ('0'(문자 0)의 아스키코드가 48!)
 			}else {
 				int rt = sk.pop();
 				int lt = sk.pop();
+				
 				if(x == '+') {
 					sk.push(lt + rt);
 				}else if(x == '-') {
@@ -24,11 +26,12 @@ public class S54 {// 후위식 연산
 				}else if(x == '*') {
 					sk.push(lt * rt);
 				}else if(x == '/') {
-					sk.push(lt/rt);
+					sk.push(lt / rt);
 				}
 			}
 		}
-		answer = sk.get(0); //수행하고나면 최종적으로 스택에는 하나의 값만 있으므로 0번 인덱스 get
+		
+		answer = sk.get(0); //for문을 수행하고나면 최종적으로 스택에는 하나의 값만 있으므로 0번 인덱스 get
 		
 		return answer;
 	}
