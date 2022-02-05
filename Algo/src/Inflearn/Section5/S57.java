b023431a8c17480ed4e142a979fb2efae2dd05cb
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-05 23:14:23 +0900

    복습 - 다시 풀기!

--- Algo/src/Inflearn/Section5/S57.java
+++ Algo/src/Inflearn/Section5/S57.java
@@ -6,24 +6,25 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class S57 {// 교육과정 설계
+public class S57 {// 교육과정 설계 - 필수과목 순서 중요!
 	public String solution(String s, String str) {
 		String answer = "YES";
 
 		Queue<Character> q = new LinkedList<>();
-		for (char x : s.toCharArray()) {
+		
+		for(char x : s.toCharArray()) { //필수과목 s를 q에 setting!
 			q.offer(x);
 		}
-
-		for (int i = 0; i < str.length(); i++) {
-			if (q.contains(str.charAt(i))) {// str안에 s가 포함되어 있는 것 뿐만 아니라 순서도 같은 순서로 있어야함
-				if (str.charAt(i) != q.poll()) {
+		for(int i = 0; i<str.length(); i++) {
+			if(q.contains(str.charAt(i))) {
+				//***if문 생각해내기 어려움.. ***
+				if(str.charAt(i) != q.poll()) {//str 안에 s가 있지만 순서가 다르다면 NO!
 					return "NO";
 				}
 			}
 		}
-		if (!q.isEmpty()) {
-			return "NO";
+		if(!q.isEmpty()) {
+			return "NO"; //q가 비어있지 않다면 str이 필수과목인 s를 모두 포함하고 있지는 않다는 의미이므로 NO!
 		}
 		return answer;
 	}
