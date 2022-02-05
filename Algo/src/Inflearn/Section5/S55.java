2381f7007d94e768bc9acecab0aa0c9e87a32054
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-05 22:37:02 +0900

    문제 해결(복습!)

--- Algo/src/Inflearn/Section5/S55.java
+++ Algo/src/Inflearn/Section5/S55.java
@@ -5,33 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class S55 {
+public class S55 {// 쇠막대기
 	public int solution(String s) {
 		int answer = 0;
-		
+
 		Stack<Character> sk = new Stack<>();
-		for(int i = 0; i<s.length(); i++) {
-			if(s.charAt(i) == '(') {
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') {
 				sk.push(s.charAt(i));
-			}else {
+			} else {
 				sk.pop();
-				if(s.charAt(i-1) == '(') { //레이저 일 때 -->()모양일 때
+				if (s.charAt(i - 1) == '(') {//else의 경우는 s.charAt(i)의 값이 ')'인데 i-1의 값이 '('라는 것은 
+					                         // '()'의 모양이라는 의미 -> 레이저!
 					answer += sk.size();
-				}else {//막대기의 끝일 때
+				} else {//s.charAt(i - 1)의 값이 ')'라는 것은 쇠막대기의 끝이라는 의미!
 					answer++;
 				}
+
 			}
 		}
-		
+
 		return answer;
 	}
 
 	public static void main(String[] args) throws IOException {
 		S55 S = new S55();
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
-		
+
 		System.out.println(S.solution(s));
 
 	}
