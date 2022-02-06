11f7ae1efa6a30987eb301f185c996f546ac85f2
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-06 16:59:17 +0900

    다시 풀기

--- Algo/src/Inflearn/Section5/S58.java
+++ Algo/src/Inflearn/Section5/S58.java
@@ -7,13 +7,39 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class S58 {
+class Person{// q삽입 순서(id)와 우선순위(priority)를 위한 person 클래스 생성
+	int id;
+	int priority;
+	public Person(int id, int priority) {
+		this.id = id;
+		this.priority = priority;
+	}	
+}
+
+public class S58 {// 응급실
 	public int solution(int n, int m, int[] arr) {
 		int answer = 0;
 		
-		Queue<Integer> q = new LinkedList<>();
-		for(int x : arr) {
-			q.offer(x);
+		Queue<Person> q = new LinkedList<>();
+		
+		for(int i = 0; i<n; i++) {// q에 (id, priority) 데이터 삽입!
+			q.offer(new Person(i, arr[i]));
+		}
+		while(!q.isEmpty()) {
+			Person check = q.poll();
+			for(Person x : q) {
+				if(x.priority > check.priority) {// check의 우선순위보다 q에 있는 다른값의 우선순위가 높으면
+					q.offer(check); // check를 다시 q에 삽입 
+					check = null; // check값 초기화
+					break;
+				}
+			}
+			if(check != null) {//check가 null이 아니라는 것은 check가 가장 우선으로 진료받는 경우
+				answer++;
+				if(check.id == m) {// m이 몇번 째로 진료를 받는 것인 지 구하는 것이므로
+					return answer;
+				}
+			}
 		}
 		
 		return answer;
