19242a7e2549bce7dc8ef917067431796ab427c1
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-14 23:01:31 +0900

    복습(다시 풀기)

--- Algo/src/Inflearn/Section5/S53.java
+++ Algo/src/Inflearn/Section5/S53.java
@@ -19,18 +19,16 @@ public class S53 {// 인형 뽑기
 					int doll = board[i][pos - 1];
 					board[i][pos - 1] = 0;
 
-					// 뽑은 인형이 스택 상단에 있는 인형과 같은 것일 경우
+					// 뽑은 인형이 스택 상단에 있는 것과 같을 경우
 					if (!sk.isEmpty() && sk.peek() == doll) { // peek(): 스택 상단에 있는 값 확인!(꺼내지 않음!)
 						answer += 2;
-						sk.pop(); // pop()은 스택 상단에 있는 값 확인 + 꺼냄!
-					} else { // 다른 경우에 스택에 push!
+						sk.pop();
+					} else {// 다른 경우에 스택에 push!
 						sk.push(doll);
 					}
 					break; // 첫번째 if문이 참일 때 한 번 수행! break 안해주면 i-for문 계속 수행하게 되므로 오답!
 				}
-
 			}
-
 		}
 
 		return answer;
