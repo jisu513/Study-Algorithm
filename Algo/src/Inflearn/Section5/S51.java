998bb8e6d97b8f9a995f61b9c27ea3c58d826fbf
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-14 21:40:30 +0900

    복습

--- Algo/src/Inflearn/Section5/S51.java
+++ Algo/src/Inflearn/Section5/S51.java
@@ -3,35 +3,36 @@ package Inflearn.Section5;
 import java.io.*;
 import java.util.*;
 
-public class S51 {//올바른 괄호
+public class S51 {// 올바른 괄호
 	public String solution(String s) {
 		String answer = "YES";
-		
-		Stack<Character> st =new Stack<>();
-		for(char x : s.toCharArray()) {
-			if(x == '(') {
+
+		Stack<Character> st = new Stack<>();
+		for (char x : s.toCharArray()) {
+			if (x == '(') {
 				st.push(x);
-			}else {
-				if(st.isEmpty()) { // '('이 있어야 ')'을 pop()하는데 stack이 비어있다는 것은 ')'가 더 많다는 의미!
+			} else {
+				if (st.isEmpty()) { //*** ')'가 있는데 스택이 비어있다면 ')'가 더 많다는 뜻! ***
 					return "NO";
 				}
 				st.pop();
 			}
 		}
-		
-		if(!st.isEmpty()) {//'('와 ')'이 짝에 맞게 pop했는데 stack이 비어있지 않다면 '('이 더 많다는 의미!
+
+		if (!st.isEmpty()) { //for문 이후에도 스택이 비어있지 않다면 '(' 이 더 많다는 뜻!
 			return "NO";
 		}
+
 		return answer;
 	}
 
 	public static void main(String[] args) throws IOException {
 		S51 S = new S51();
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String s = br.readLine();
-		
+
 		System.out.println(S.solution(s));
 
 	}
