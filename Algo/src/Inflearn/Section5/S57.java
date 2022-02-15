a38905e013cff5ac98376f21dc4a80b26d220e46
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-15 22:42:14 +0900

    복습 - 다시

--- Algo/src/Inflearn/Section5/S57.java
+++ Algo/src/Inflearn/Section5/S57.java
@@ -11,19 +11,20 @@ public class S57 {// 교육과정 설계 - 필수과목 순서 중요!
 		String answer = "YES";
 
 		Queue<Character> q = new LinkedList<>();
-		for(char x : s.toCharArray()) { // 필수과목 q에 넣기
+		// 필수과목 q에 넣기
+		for(char x : s.toCharArray()) { 
 			q.offer(x);
 		}
 		for(int i = 0; i<str.length(); i++) {
 			if(q.contains(str.charAt(i))) {
-				if(str.charAt(i) != q.poll()) {// **poll()한 값과 같지 않다면 순서가 다르다는 의미이므로 No를 리턴!(s = "abc" str = "cba"이면 NO)
+				if(q.poll() != str.charAt(i)) {
+					// **poll()한 값과 같지 않다면 순서가 다르다는 의미이므로 No를 리턴!(s = "abc" str = "cba"이면 NO)
 					return "NO";
 				}
 			}
 		}
-		
-		if(!q.isEmpty()) { // q가 비어있지 않다면 str에 포함되지 못한 s가 있다는 의미이므로 No 리턴!
-			return "NO";
+		if(!q.isEmpty()) {  // q가 비어있지 않다면 str에 포함되지 못한 s가 있다는 의미이므로 No 리턴!
+			return " NO";
 		}
 		
 		return answer;
