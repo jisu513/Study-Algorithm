de98e5f90a9de4ccca12853e704a88615d40e6ce
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-15 21:51:22 +0900

    복습 - 다시

--- Algo/src/Inflearn/Section5/S56.java
+++ Algo/src/Inflearn/Section5/S56.java
@@ -12,22 +12,26 @@ public class S56 {// 공주 구하기
 		int answer = 0;
 
 		/*
-		 * Queue(First In First Out) : 먼저 들어오 데이터가 먼저 나감! offer(): Queue에 데이터를 집어 넣는 함수
-		 * poll() : Queue에서 데이터를 빼내는 함수 size() : Queue의 크기 구하는 함수 contanins(*) : *값이
-		 * Queue에 있는지의 유무를 T/F로 리턴하는 함수
+		 * Queue(First In First Out) : 먼저 들어오 데이터가 먼저 나감! 
+		 * offer(): Queue에 데이터를 집어 넣는 함수
+		 * poll() : Queue에서 데이터를 빼내는 함수 
+		 * size() : Queue의 크기 구하는 함수 
+		 * contanins(#) : #값이 Queue에 있는지의 유무를 T/F로 리턴하는 함수
 		 */
 
 		Queue<Integer> q = new LinkedList<>();
-
-		for (int i = 0; i < n; i++) {// q에 1~n까지 숫자 삽입
+		//q에 1~n까지의 숫자 offer()
+		for (int i = 0; i < n; i++) {
 			q.offer(i + 1);
 		}
 		while (!q.isEmpty()) {
-			for (int i = 1; i < k; i++) {// 특정 숫자 k전까지의 데이터 빼내서 다시 q에 집어 넣기!
+			// k바로 전의 숫자까지 poll()하고  다시 offer()
+			for (int i = 1; i < k; i++) {
 				q.offer(q.poll());
 			}
-			q.poll(); // k 번째 데이터 poll()
-			if (q.size() == 1) {// q안에 데이터가 하나 남아있을 때 값이 answer!
+			//k번째 poll()
+			q.poll();
+			if (q.size() == 1) {
 				answer = q.poll();
 			}
 		}
