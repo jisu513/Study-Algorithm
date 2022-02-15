1f64522ba9ca36c4229aece7a9f08b2be207bf23
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-15 21:06:46 +0900

    복습

--- Algo/src/Inflearn/Section5/S53.java
+++ Algo/src/Inflearn/Section5/S53.java
@@ -12,18 +12,19 @@ public class S53 {// 인형 뽑기
 
 		Stack<Integer> sk = new Stack<>();
 
-		for (int pos : moves) {// moves의 값이 크레인의 위치!
-			for (int i = 0; i < board.length; i++) {// board.length는 행의 길이! board[0].length는 열의 길이
-				if (board[i][pos - 1] != 0) {// board의 index는 0부터 시작하므로 moves값(=pos)에서 -1 해줘야함!
+		for (int pos : moves) { // moves의 값이 크레인의 위치!
+			for (int i = 0; i < board.length; i++) { // board.length는 행의 길이 (board[0].length는 열의 길이)
+				if (board[i][pos - 1] != 0) { // board의 index는 0부터 시작하므로 moves값(=pos)에서 -1 해줘야함!
 					// 인형 뽑기
 					int doll = board[i][pos - 1];
 					board[i][pos - 1] = 0;
 
 					// 뽑은 인형이 스택 상단에 있는 것과 같을 경우
-					if (!sk.isEmpty() && sk.peek() == doll) { // peek(): 스택 상단에 있는 값 확인!(꺼내지 않음!)
+					if (!sk.isEmpty() && sk.peek() == doll) {
+						// peek(): 스택 상단에 있는 값 확인!(꺼내지 않음!)
 						answer += 2;
 						sk.pop();
-					} else {// 다른 경우에 스택에 push!
+					} else { // 다른 경우에 스택에 push!
 						sk.push(doll);
 					}
 					break; // 첫번째 if문이 참일 때 한 번 수행! break 안해주면 i-for문 계속 수행하게 되므로 오답!
