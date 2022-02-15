255868b0b76cb082d8507da80af052d89dee7fc6
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-15 21:43:21 +0900

    복습

--- Algo/src/Inflearn/Section5/S55.java
+++ Algo/src/Inflearn/Section5/S55.java
@@ -10,14 +10,15 @@ public class S55 {// 쇠막대기
 		int answer = 0;
 
 		Stack<Character> sk = new Stack<>();
+
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) == '(') {
 				sk.push(s.charAt(i));
 			} else {
 				sk.pop();
-				if (s.charAt(i - 1) == '(') { // '()' 모양이므로 구슬! 
+				if (s.charAt(i - 1) == '(') { // '()' 모양이므로 구슬!
 					answer += sk.size();
-				} else { // 막대기의 끝을 의미!
+				} else {// 막대기의 끝을 의미!
 					answer++;
 				}
 			}
