a8a2f1460d4485dcf7789f70cc6f3cd92b25ede1
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-17 20:25:09 +0900

    문제 해결 - 복습

--- Algo/src/Inflearn/Section5/S56.java
+++ Algo/src/Inflearn/Section5/S56.java
@@ -12,25 +12,26 @@ public class S56 {// 공주 구하기
 		int answer = 0;
 
 		/*
-		 * Queue(First In First Out) : 먼저 들어오 데이터가 먼저 나감! 
-		 * offer(): Queue에 데이터를 집어 넣는 함수
-		 * poll() : Queue에서 데이터를 빼내는 함수 
-		 * size() : Queue의 크기 구하는 함수 
-		 * contanins(#) : #값이 Queue에 있는지의 유무를 T/F로 리턴하는 함수
+		 * Queue(First In First Out) : 먼저 들어오 데이터가 먼저 나감! offer(): Queue에 데이터를 집어 넣는 함수
+		 * poll() : Queue에서 데이터를 빼내는 함수 size() : Queue의 크기 구하는 함수 contanins(#) : #값이
+		 * Queue에 있는지의 유무를 T/F로 리턴하는 함수
 		 */
 
 		Queue<Integer> q = new LinkedList<>();
+
 		//q에 1~n까지의 숫자 offer()
 		for (int i = 0; i < n; i++) {
 			q.offer(i + 1);
 		}
+		
 		while (!q.isEmpty()) {
-			// k바로 전의 숫자까지 poll()하고  다시 offer()
+			// k바로 전의 숫자까지 poll()하고  다시 offer() ** 1부터 시작하므로 int i = 1 **
 			for (int i = 1; i < k; i++) {
 				q.offer(q.poll());
 			}
 			//k번째 poll()
 			q.poll();
+			
 			if (q.size() == 1) {
 				answer = q.poll();
 			}
