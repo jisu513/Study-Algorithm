1b615166b4c687b4493fa322f55c091a3cd506f6
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-23 19:23:49 +0900

    문제 해결

---
+++ Algo/src/Inflearn/Section6/S65.java
@@ -0,0 +1,53 @@
+package Inflearn.Section6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class S65 {
+	public char solution(int n, int[] arr) {
+		char answer = 'U';
+
+		// 해시맵 사용
+		/* HashMap<Integer, Integer> hm = new HashMap<>();
+		for (int x : arr) {
+			hm.put(x, hm.getOrDefault(x, 0) + 1);
+		}
+
+		for(int x : hm.keySet()) {
+			if(hm.get(x) >= 2) {
+				return 'D';
+			}
+		}*/
+		
+		// 정렬 사용
+		Arrays.sort(arr);
+		for(int i = 0; i<n-1; i++) {
+			if(arr[i] == arr[i+1]) {
+				return 'D';
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) throws IOException {
+		S65 S = new S65();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		int n = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(S.solution(n, arr));
+	}
+
+}
