d6d7d23945b0053c94c4ac61cb7698c36d250100
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-24 19:30:23 +0900

    복습 - 다시 풀기

--- Algo/src/Inflearn/Section6/S64.java
+++ Algo/src/Inflearn/Section6/S64.java
@@ -11,19 +11,21 @@ public class S64 {// Least Recently Used
 
 		for (int x : arr) {
 			int pos = -1; // pos는 hit 일 때 해당 answer[]의 index이므로 arr[] index에 해당하지 않는 -1 (0 ~ s-1 제외)로 설정!
-			for (int i = 0; i < s; i++) { // answer[i]에 x가 있는 지 확인하는 과정 --> 있으면 hit, 없으면 miss
+			
+			// hit인지 아닌지 확인 --> pos의 값이 변하지 않으면 miss / 변하면 hit
+			for (int i = 0; i < s; i++) {
 				if (x == answer[i]) { // hit일 때
 					pos = i; // 해당 index값을 pos 넣어줌
 				}
 			}
+			
 			if (pos == -1) { // miss인 경우
 				for (int i = s - 1; i >= 1; i--) { // 맨 마지막 index에서 감소하면서 자리옮기기! (첫번째 index [0]는 새로운 input!)
 					answer[i] = answer[i - 1]; // ex) answer[3]에 answer[2] 값 들어옴 --> 한 칸 씩 옆으로 이동!
 					// answer[0] = x;
-
 				}
-			} else { // hit인 경우 
-				for (int i = pos; i >= 1; i--) { // hit난 자리의 index부터 감소하면서 자리옮기기!
+			} else { // hit인 경우
+				for (int i = pos; i >= 1; i--) { // hit난 자리의 index(pos)부터 감소하면서 자리옮기기!
 					answer[i] = answer[i - 1];
 					// answer[0] = x;
 				}
