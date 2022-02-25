b43a9ad040ac57fd1747e45960de6c952b52df5b
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-25 23:40:33 +0900

    문제 해결

--- Algo/src/Baekjoon/BJ9012.java
+++ Algo/src/Baekjoon/BJ9012.java
@@ -3,11 +3,27 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
-public class BJ9012 {//°ýÈ£
+public class BJ9012 {// °ýÈ£
 	public static String solution(String s) {
-		String answer="Yes";
-		return answer;
+		Stack<Character> st = new Stack<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') {
+				st.push(s.charAt(i));
+			} else {
+				if (st.isEmpty()) {
+					return "NO";
+				}
+				st.pop();
+			}
+		}
+		if (!st.isEmpty()) {
+			return "NO";
+		}
+
+		return "YES";
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -15,18 +31,13 @@ public class BJ9012 {//°ýÈ£
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+
 		int n = Integer.parseInt(br.readLine());
-		for(int i = 0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
 			sb.append(solution(br.readLine())).append('\n');
 		}
-		
-		System.out.println(sb);
 
-		
-		
-		
-		
+		System.out.println(sb);
 
 	}
 
