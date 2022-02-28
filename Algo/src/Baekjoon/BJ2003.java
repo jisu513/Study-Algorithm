e26c6aef89d4762d9537e6f9d6256ec3d23f6d6c
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-02-28 22:39:13 +0900

    해결

---
+++ Algo/src/Baekjoon/BJ2003.java
@@ -0,0 +1,44 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2003 { //Inflearn S34¿Í µ¿ÀÏ
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int lt = 0, sum = 0, cnt = 0;
+		for (int rt = 0; rt < n; rt++) { 
+			sum += arr[rt];
+			if (sum == m) {
+				cnt++;
+			}
+			while (sum >= m) { 
+				sum -= arr[lt];
+				lt++;
+				if(sum == m) {
+					cnt++;
+				}
+
+			}
+		}
+		System.out.println(cnt);
+
+	}
+
+}
