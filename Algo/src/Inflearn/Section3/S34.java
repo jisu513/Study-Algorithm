6b810b4643ff2aca42f37a3aaf3ef0cf2ddec5a6
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-03-09 23:01:56 +0900

    해결

--- Algo/src/Inflearn/Section3/S34.java
+++ Algo/src/Inflearn/Section3/S34.java
@@ -11,11 +11,11 @@ public class S34 {// 연속 부분수열(합이 특정숫자 m이 되는 갯수)
 
 		int sum = 0, lt = 0;
 		for (int rt = 0; rt < n; rt++) {
-			sum += arr[rt];
+			sum += arr[rt]; //lt는 더해줄 필요 없음! arr[0]이 두번 더해지는 꼴 (+ arr[lt]가 계속 누적해서 더해짐)
 			if (sum == m) {
 				answer++;
 			}
-			while (sum >= m) {//**sum이 m일때도 while문 돌아야함!
+			while (sum >= m) {//arr[lt]를 한 번 빼서는 안되는 경우도 있기 때문에 while문 사용 (**sum이 m일때도 while문 돌아야함!)
 				sum -= arr[lt];
 				lt++;
 				if (sum == m) {//**sum > m일 때 arr[lt]를 뺀 값이 m일 되는 경우도 있음!
