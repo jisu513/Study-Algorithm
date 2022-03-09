c4b0ac6ae5fc7d2fed6c7dc37e4921a0ecd8a058
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-03-09 22:53:25 +0900

    다시 풀어보기

---
+++ Algo/src/Inflearn/Section6/S610.java
@@ -0,0 +1,63 @@
+package Inflearn.Section6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S610 {// 마구간 정하기(결정 알고리즘)
+	public int count(int[] arr, int dist) {
+		int cnt = 1; // 배치한 말의 개수
+		int end_p = arr[0]; // 말이 배치된 좌표(처음에는 가장 왼쪽 좌표에 첫번째 말 배치 --> 말은 많이 배치할 수 있는 방법)
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] - end_p >= dist) {// 말을 배치하려고 하는 좌표와 이미 말이 들어있는 좌표 사이의 거리가 dist(=mid) 보다 크거나 같아야함
+				cnt++;
+				end_p = arr[i]; // 말이 배치된 좌표 업데이트ㄹ
+			}
+		}
+		return cnt;
+
+	}
+
+	public int solution(int n, int c, int[] arr) {
+		int answer = 0;
+
+		Arrays.sort(arr); // 이진검색은 반드시 정렬이 되어있어야 함!
+		int lt = 1; // 마구간 사이의 최소거리
+		int rt = arr[n - 1]; // 가장 마지막 좌표 값
+
+		while (lt <= rt) {
+			int mid = (lt + rt) / 2;
+			if (count(arr, mid) >= c) {
+				answer = mid;
+				lt = mid + 1;
+			} else {
+				rt = mid - 1;
+			}
+		}
+		return answer;
+	}
+
+	public static void main(String[] args) throws IOException {
+		S610 S = new S610();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		System.out.println(S.solution(n, c, arr));
+
+	}
+
+}
