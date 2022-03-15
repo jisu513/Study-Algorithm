7c8379eb7b776e690efa418c8cf970a3f7f64c1a
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-03-15 20:00:24 +0900

    복습

--- Algo/src/Inflearn/Section6/S610.java
+++ Algo/src/Inflearn/Section6/S610.java
@@ -7,29 +7,33 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class S610 {// 마구간 정하기(결정 알고리즘)
-	public int count(int[] arr, int dist) {
-		int cnt = 1; // 배치한 말의 개수
-		int ep = arr[0]; // 말이 배치된 좌표 (첫 말은 배열의 첫번째에 배치되는 것이 가장 좋음)
+	public int count(int[] arr, int distance) {
+		int cnt = 1;
+		int end_p = arr[0]; // end_p는 말이 배치된 좌표! (첫번째 말은 첫번쨰 좌표에 배치!)
 
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] - ep >= dist) { // 말을 배치하려고 하는 좌표와 이미 말이 들어있는 좌표 사이의 거리가 dist(=mid) 보다 크거나 같아야함
+			if (arr[i] - end_p >= distance) {
+				// 말을 배치하려고 하는 좌표와 이미 말이 들어있는 좌표 사이의 거리가 distance(=mid) 보다 크거나 같아야함
 				cnt++;
-				ep = arr[i]; // 말이 배치된 좌표 업데이트
+				end_p = arr[i]; // 말이 배치된 좌표 업데이트
 			}
 		}
 		return cnt;
+
 	}
 
 	public int solution(int n, int c, int[] arr) {
 		int answer = 0;
 
-		Arrays.sort(arr); // 이진검색은 반드시 정렬이 되어있어야 함!
-		int lt = 1; // 마구간 사이의 최소거리
+		Arrays.sort(arr); // ***이진검색은 반드시 정렬이 되어있어야 함!***
+
+		int lt = 1; // 두 말사이의 최소거리
 		int rt = arr[n - 1]; // 가장 마지막 좌표 값
 
 		while (lt <= rt) {
-			int mid = (lt + rt) / 2; // 두 말의 최대거리
+			int mid = (lt + rt) / 2;
 			if (count(arr, mid) >= c) {
+				// count()는 말이 배치된 갯수 -> c(배치되어야 하는 말)보다 크다면 두 말사이의 거리(mid)가 충분하다는 것! (더 먼 거리에서 다시 확인!)
 				answer = mid;
 				lt = mid + 1;
 			} else {
