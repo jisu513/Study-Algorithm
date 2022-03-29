1e327e962e4d463fae002cfd81c750a4cbca8f60
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-03-29 23:18:09 +0900

    그래프 최단거리(BFS)

--- Algo/src/Inflearn/Section7/S713.java
+++ Algo/src/Inflearn/Section7/S713.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+
 public class S713 { // 그래프 최단거리 (BFS) **최단거리는 BFS 사용!
 	static int n, m;
 	static ArrayList<ArrayList<Integer>> graph;
@@ -12,16 +13,16 @@ public class S713 { // 그래프 최단거리 (BFS) **최단거리는 BFS 사용!
 
 	public void BFS(int v) {
 		Queue<Integer> q = new LinkedList<>();
-		ch[v] = 1;
+		ch[v] = 1; 
 		dis[v] = 0;
 		q.offer(v);
 		while (!q.isEmpty()) {
-			int cv = q.poll();
-			for (int nv : graph.get(cv)) {
-				if (ch[nv] == 0) {
+			int cv = q.poll(); // 현재 정점
+			for (int nv : graph.get(cv)) { // nv는 다음 정점!
+				if (ch[nv] == 0) { // 방문 유무 체크! 안했으면 if문 수행
 					ch[nv] = 1;
 					q.offer(nv);
-					dis[nv] = dis[cv] + 1;
+					dis[nv] = dis[cv] + 1; // *** cv의 간선을 타고 온것이기 때문에 dis[cv]+1 해줌!
 				}
 			}
 		}
@@ -43,7 +44,7 @@ public class S713 { // 그래프 최단거리 (BFS) **최단거리는 BFS 사용!
 		}
 
 		ch = new int[n + 1]; // 방문했는지 유무 check!
-		dis = new int[n + 1];
+		dis = new int[n + 1]; // dis[n]은 1번 간선에서 n번으로 가는데 걸린 최소거리!
 
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
