ce177afd5eb16de46cda442d14d0898f855be9bb
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-04-05 20:52:55 +0900

    복습 (DFS 활용) // S801과 흡사한 문제

--- Algo/src/Inflearn/Section8/S802.java
+++ Algo/src/Inflearn/Section8/S802.java
@@ -1,32 +1,41 @@
 package Inflearn.Section8;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class S802 {
+public class S802 { // 바둑이 승차 (DFS)
 
-	static int answer = Integer.MIN_VALUE, c, n;
+	static int answer = Integer.MIN_VALUE, c, n; //c는 최대 무게
 
 	public void DFS(int L, int sum, int[] arr) {
-		if (sum > c)
+		if (sum > c) 
 			return;
-		if (L == n) {
+		if (L == n) { // 인덱스는 n-1번까지 --> L==n일 때 ** 부분 집합 완성 ** 
 			answer = Math.max(answer, sum);
-		} else {
+		} else { // 부분 집합 구하기
 			DFS(L + 1, sum + arr[L], arr);
 			DFS(L + 1, sum, arr);
 		}
 	}
 
-	public static void main(String[] args) {
-		S802 T = new S802();
-		Scanner kb = new Scanner(System.in);
-		c = kb.nextInt();
-		n = kb.nextInt();
+	public static void main(String[] args) throws IOException {
+		S802 S = new S802();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		st = new StringTokenizer(br.readLine());
+		c = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		
 		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
-			arr[i] = kb.nextInt();
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		T.DFS(0, 0, arr);
+		
+		S.DFS(0, 0, arr); // (int L, int sum, int[] arr)
 		System.out.println(answer);
 	}
 }
