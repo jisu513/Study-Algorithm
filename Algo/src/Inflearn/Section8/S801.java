ff0d61e85c7e71e468a6be3a00ccf2339e1eb49d
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-04-06 18:34:04 +0900

    DFS 활용

--- Algo/src/Inflearn/Section8/S801.java
+++ Algo/src/Inflearn/Section8/S801.java
@@ -5,28 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class S801 { // 합이 같은 부분집합
-	static String answer = "NO";
-	static int n, total = 0;
-	boolean flag = false; // answer = "YES" 가 발견됐을 때 그 다음 재귀들은 모두 return 시키기 위한 하나의 장치
+public class S801 { // 합이 같은 부분집합 (DFS)
+	static int n, total = 0; // total은 arr의 총 합
+	static String answer = "No";
+	boolean flag = false; // answer = "YES" 가 발견됐을 때 그 다음 재귀들은 모두 return 시키기 위한 표시
 
-	public void DFS(int L, int sum, int[] arr) {
-		if(flag) {
+	public void DFS(int L, int sum, int[] arr) { // sum은 부분집합의 총합
+		if (flag) {
 			return;
 		}
-		if(sum > total/2) { // 더 해볼 필요 없이 answer = NO (두 부분집합의 합이 같을 수가 없으므로)
+		if (sum > total / 2) { // 부분집합(sum)의 총합이 total/2보다 크면 더 해볼 필요 X (합이 같은 부분집합을 찾고 있기때문에!)
 			return;
 		}
-		if (L == n) { // 인덱스는 n-1번까지 --> L==n일 때 ** 부분 집합 완성 ** 
-			if ((total - sum) == sum) { // total은 arr의 총합, sum은 부분집합의 합
-				answer = "YES"; 
+		if (n == L) { // index는 0 ~ n-1 까지 -> n==L이면 부분집합 완성
+			if ((total - sum) == sum) { // 두 부분집합의 합이 같으므로
+				answer = "YES";
 				flag = true;
 			}
-		} else { // 부분 집합 구하기
-			DFS(L + 1, sum + arr[L], arr); // arr[L] 포함
-			DFS(L + 1, sum, arr); // arr[L] 불포함
-		}
-
+		} else {
+			DFS(L + 1, sum + arr[L], arr); // 아이패드로 확인!  O(포함)
+			DFS(L + 1, sum, arr); // X(불포함)
+		} 
 	}
 
 	public static void main(String[] args) throws IOException {
