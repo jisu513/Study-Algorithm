05e3558a22c472f4ec0f8c001ca12c0c9790e278
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-04-18 22:23:47 +0900

    송아지 찾기(BFS) - 복습

--- Algo/src/Inflearn/Section7/S78.java
+++ Algo/src/Inflearn/Section7/S78.java
@@ -8,38 +8,36 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class S78 {// 송아지 찾기 (BFS)
-	int answer = 0;
-	int[] dist = {1, -1, 5};
-	int[] check; // 이미 한 번 추가한 자식노드와 동일한 값의 다른 자식노드를 또 추가하는 없게 하기 위해 체크!
+	static int[] dis = { 1, -1, 5 }; // 한 번에 이동할 수 있는 거리
+	static int[] check; // 방문 했던 노드에 또 다시 방문하지 않을 수 있게 방문 했는지 유무 check
 	Queue<Integer> Q = new LinkedList<>();
+
 	public int BFS(int s, int e) {
-		check = new int[10001]; // 좌표 범위 1~10000
-		check[s]  = 1; // 내 위치에 대한 좌표 체크
-		Q.offer(s);
-		int L = 0; // 노드의 Level
-		
-		while(!Q.isEmpty()) {
-			int len = Q.size();
-			for(int i = 0; i<len; i++) { //Q의 크기만큼 반복
-				int current = Q.poll(); // 현재 노드
-				for(int j = 0; j<dist.length; j++) { 
-					int child = current + dist[j]; // dist[] 배열 값 만큼 이동한(더한) 값 자식노드로 설정
-					if(child == e) { // 송아지 찾았는지 확인!
-						return L+1; // chiled는 자식노드 이고 현재 Level은 부모노드에 맞춰있기 때문에 +1 해줘야함!
+		check = new int[100001]; // 좌표 1 ~ 10000 까지
+		check[s] = 1; // 시작위치 check
+		Q.offer(s); // 시작 위치(사람 위치) Q에 삽입
+		int L = 0; // Level
+
+		while (!Q.isEmpty()) {
+			int len = Q.size(); // Level마다의 노드 개수
+			for (int i = 0; i < len; i++) {
+				int x = Q.poll(); // 현재 노드
+				for (int j = 0; j < dis.length; j++) {
+					int nx = x + dis[j]; // 자식 노드
+					if (nx == e) {// 송아지 위치와 같으면 리턴! (현재 노드의 Level은 L, nx는 자식노드이므로 L+1 해주기!)
+						return L + 1;
 					}
-					if(current >= 1 && current <= 10000 && check[child] == 0) {
-						check[child] = 1; // 자식노드 값 체크
-						Q.offer(child);
+					if (nx >= 1 && nx <= 10000 && check[nx] == 0) {
+						// 자식노드가 if문에 부합하면 Q에 삽입! (이미 방문했던 노드는 다시 방문X)
+						check[nx] = 1;
+						Q.offer(nx);
 					}
 				}
 			}
 			L++;
 		}
-		
 		return 0;
-		
 	}
-
 
 	public static void main(String[] args) throws IOException {
 		S78 S = new S78();
@@ -47,8 +45,8 @@ public class S78 {// 송아지 찾기 (BFS)
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int s = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken()); // 사람 위치
+		int e = Integer.parseInt(st.nextToken()); // 송아치 위치
 
 		System.out.println(S.BFS(s, e));
 
