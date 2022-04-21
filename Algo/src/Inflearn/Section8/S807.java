dce8727d5224ffb32259a948c532a39cacca3f15
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-04-21 21:24:37 +0900

    조합의 수 (DFS) - 메모이제이션(S74 피보나치수열)

--- Algo/src/Inflearn/Section8/S807.java
+++ Algo/src/Inflearn/Section8/S807.java
@@ -10,22 +10,26 @@ public class S807 { // 조합의 경우의 수(메모이제이션) nCr = n-1Cr-1 + c-1Cr
 	/* 동일한 계산을 반복해야 할 경우 한 번 계산한 결과를 메모리에 저장해 두었다가 꺼내 씀으로써 
 	 * 중복 계산을 방지할 수 있게 하는 기법 -> 동적 계획법의 핵심이 되는 기술
 	 */
+	
 	static int n, r;
-	int[][] dy = new int[35][35]; // 메모이제이션을 위해 사용
+	int[][] dy = new int[35][35]; // 메모이제이션을 위해 사용 (없으면 성능 굉장히 저하됨) [35]까지 한 이유는 문지에서 주어져서임
 	public int DFS(int n, int r) {
+		// 메모이제이션 사용
 		if(dy[n][r] > 0) {
 			return dy[n][r];
 		}
+		
+		// 조합의 수 구하기
 		if(n == r || r == 0) {
 			return 1;
 		}else {
-			return dy[n][r] =  DFS(n-1, r-1) + DFS(n-1, r);
+			return dy[n][r] =  DFS(n-1, r-1) + DFS(n-1, r); // 조합: nCr = n-1Cr-1 + c-1Cr
 		}
 		
 	}
 	
 	// 메모이제이션 X --> 성능 매우 저하됨
-	/*
+	/*  조합: nCr = n-1Cr-1 + c-1Cr
 	 * public int DFS(int n, int r) {
 		if(n == r || r == 0) {
 			return 1;
