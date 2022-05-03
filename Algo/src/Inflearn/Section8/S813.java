8ade3720824cfb820ca7b7be2a8efca05cd3d121
DESKTOP-6V3RSAT\USER-PC <youjs513@naver.com>
2022-05-03 23:15:28 +0900

    다시 공부

--- Algo/src/Inflearn/Section8/S813.java
+++ Algo/src/Inflearn/Section8/S813.java
@@ -6,6 +6,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class S813 {
+	static int n, answer = 0;
+	// 시계방향으로 돌기 위한 방향배열
+	static int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
+	static int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
+
+	public void DFS(int x, int y, int[][] board) {
+		for (int i = 0; i < 8; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+				board[nx][ny] = 0;
+				DFS(nx, ny, board);
+			}
+		}
+
+	}
+
+	public void solution(int[][] board) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][j] == 1) {
+					answer++;
+					board[i][j] = 0;
+					DFS(i, j, board);
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		S813 S = new S813();
@@ -13,15 +41,18 @@ public class S813 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 
-		int n = Integer.parseInt(br.readLine());
+		n = Integer.parseInt(br.readLine());
 
-		int[][] board = new int[n][n];
+		int[][] arr = new int[n][n];
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < n; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+
+		S.solution(arr);
+		System.out.println(answer);
 
 	}
 
